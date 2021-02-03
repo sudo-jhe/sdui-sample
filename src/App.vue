b7205386026706a2312c8dd1f5b6e65ad514b68d
faqudlx <nibbla@gmail.com>
2021-02-03 19:13:28 +0100

    Init

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
+
+
 export default {
   name: 'App',
   components: {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import PortalVue from 'portal-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(Vuex)
+Vue.use(PortalVue)  // Bootstrap Toast
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
